9fdd6a8f257b01c0ac0ae97f85e5685fa5bb3ac8
Alyssa Beaton Tanguay <ehseebe@gmail.com>
2020-11-14 15:18:01 -0500

    setSelectedProject

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -4,16 +4,18 @@ import axios from 'axios';
 const ProjectList = () => {
 
   const [projects, setProjects] = useState([]);
+  const [selectedProject, setSelectedProject] = useState(null);
 
   useEffect(() => {
     const getProjects = async () => {
       const response = await axios.get("http://localhost:10013/wp-json/wp/v2/project")
       setProjects(response.data)
     }
-    getProjects()
+    getProjects(setSelectedProject(projects[projects.length-1])) //sets selected to vagary
 
   }, [])
 
+  console.log("?", selectedProject)
   // console.log("projects", projects)
   // console.log("projects in projectlist", projects)
 
