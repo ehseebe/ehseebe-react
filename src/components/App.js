089b02113844351f85154f15b1613353b023ec6b
Alyssa Beaton Tanguay <ehseebe@gmail.com>
2020-11-14 17:24:25 -0500

    implemented Router

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,38 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios'
-import ProjectList from './ProjectList';
-import PrintList from './PrintList';
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import ProjectList from "./ProjectList";
+import PrintList from "./PrintList";
+import "./App.css";
 
 const App = () => {
-  
-
-  
-
-  
-
   return (
-    <> 
-    {/* <ProjectList />  */}
-    <PrintList />
-    </>
-  )
-}
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/projects">Projects</Link>
+            </li>
+            <li>
+              <Link to="/print">Print</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/projects">
+            <ProjectList />
+          </Route>
+          <Route path="/print">
+            <PrintList />
+          </Route>
+          <Route path="/"></Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+};
 
 export default App;
