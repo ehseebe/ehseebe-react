479259711628117afeaf8e889b5e8057b99058fc
Alyssa Beaton Tanguay <ehseebe@gmail.com>
2020-11-17 10:59:22 -0500

    welcome text animations

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,22 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React from "react";
 import "./Home.css";
 
 const Home = () => {
 
 
-  return (<section>bonjour/hi!</section>);
+  return (<section className="welcome-page">
+    <section className="welcome-text">
+    <p className="letter-b bounce-top">b</p>
+    <p className="letter-o jello-horizontal">o</p>
+    <p className="letter-n bounce-bottom">n</p>
+    <p className="letter-j wobble-hor-bottom">j</p>
+    <p className="letter-o bounce-top">o</p>
+    <p className="letter-u wobble-hor-bottom">u</p>
+    <p className="letter-r jello-horizontal">r</p>
+    <p className="slash bounce-bottom">/</p>
+    <p className="letter-hi pulsate-fwd">hi!</p>
+    </section>
+    </section>);
 };
 
 export default Home;
