eb61a18596d8047a3f73b41b70cade4cf6d5f105
Alyssa Beaton Tanguay <ehseebe@gmail.com>
2020-11-17 08:28:13 -0500

    render photos

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./Home.css";
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  const [photos, setPhotos] = useState([]);
+
+  useEffect(() => {
+    const getPhotos = async () => {
+      const response = await axios.get(
+        "http://localhost:10013/wp-json/wp/v2/photography"
+      );
+      setPhotos(response.data);
+    };
+    getPhotos();
+  }, []);
+
+  console.log(photos);
+
+  const renderedPhotos = photos.map((photo) => {
+    return (
+      <figure className="photo-container" key={photo.id}>
+        <img src={photo.acf.photo} alt={photo.title.rendered} />
+      </figure>
+    );
+  });
+  return <section className="homepage-photos">{renderedPhotos}</section>;
+};
 
 export default Home;
