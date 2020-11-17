8f386907a5d94b9af15c19200f512c278a53664f
Alyssa Beaton Tanguay <ehseebe@gmail.com>
2020-11-17 11:42:29 -0500

    adjusted home vh

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,23 +2,15 @@ import React from "react";
 import "./Home.css";
 
 const Home = () => {
-
-
-  return (<section className="welcome-page">
-    <section className="welcome-text">
-    {/* <p className="letter-b bounce-top">b</p>
-    <p className="letter-o jello-horizontal">o</p>
-    <p className="letter-n bounce-bottom">n</p>
-    <p className="letter-j wobble-hor-bottom">j</p>
-    <p className="letter-o bounce-top">o</p>
-    <p className="letter-u wobble-hor-bottom">u</p>
-    <p className="letter-r jello-horizontal">r</p>
-    <p className="slash bounce-bottom">/</p> */}
-    <p className="bonjour tracking-in-expand">bonjour</p>
-    <p className="slash jello-horizontal">/</p>
-    <p className="letter-hi pulsate-fwd">hi!</p>
+  return (
+    <section className="welcome-page">
+      <section className="welcome-text">
+        <p className="bonjour tracking-in-expand">bonjour</p>
+        <p className="slash jello-horizontal">/</p>
+        <p className="letter-hi pulsate-fwd">hi!</p>
+      </section>
     </section>
-    </section>);
+  );
 };
 
 export default Home;
